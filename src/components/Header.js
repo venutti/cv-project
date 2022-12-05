9a745706d05b8ba2e1af68bd4acdd8b452901000
venutti <lvenutti@fi.uba.ar>
2022-12-05 12:44:37 -0300

    Cambia App y Header a componentes funcionales

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
 import React from "react";
 import logo from "./assets/logo.png";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="header layout">
-        <img alt="logo" className="header-logo" src={logo} />
-        <h1 className="header-title">Generador de CV</h1>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className="header layout">
+      <img alt="logo" className="header-logo" src={logo} />
+      <h1 className="header-title">Generador de CV</h1>
+    </header>
+  );
+};
 
 export default Header;
