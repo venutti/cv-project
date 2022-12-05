2d567f493a856250887652b7afb5d41691aafa03
venutti <lvenutti@fi.uba.ar>
2022-12-05 14:43:30 -0300

    Cambia utils a functional components

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-class Button extends React.Component {
-  render() {
-    const { content, onClick, type = "dark" } = this.props;
-    const className = `btn btn-${type} w-100`;
-    return (
-      <button className={className} onClick={onClick}>
-        {content}
-      </button>
-    );
-  }
+function Button(props) {
+  const { content, onClick, type = "dark" } = props;
+  const className = `btn btn-${type} w-100`;
+  return (
+    <button className={className} onClick={onClick}>
+      {content}
+    </button>
+  );
 }
 
 export default Button;
