bb1f44fb9388d34e25677d34b472fe8541cd8a96
venutti <lvenutti@fi.uba.ar>
2022-12-05 14:45:55 -0300

    Cambia estilo de componentes funcionales App y Header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 
-const App = () => {
+function App() {
   return (
     <>
       <Header></Header>
       <Main></Main>
     </>
   );
-};
+}
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "./assets/logo.png";
 
-const Header = () => {
+function Header() {
   return (
     <header className="header layout">
       <img alt="logo" className="header-logo" src={logo} />
       <h1 className="header-title">Generador de CV</h1>
     </header>
   );
-};
+}
 
 export default Header;
