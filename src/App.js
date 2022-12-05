3949e10e51f25dba80deebb579b0a1486e423904
venutti <lvenutti@fi.uba.ar>
2022-12-05 12:44:37 -0300

    Cambia App y Header a componentes funcionales

--- src/App.js
+++ src/App.js
@@ -2,30 +2,13 @@ import React from "react";
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Header></Header>
-        <Main></Main>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+    </>
+  );
+};
 
 export default App;
-
-/*
-<div>
-        <GeneralInfo
-          update={(newGeneralData) =>
-            this.setState({ ...this.state, generalData: newGeneralData })
-          }
-        />
-        <EducationInfo
-          update={(newEducationData) =>
-            this.setState({ ...this.state, educationData: newEducationData })
-          }
-        />
-      </div>
-*/
